295d6c5e004d0dd6c5240b505912286a941ad19c
hyauns123 <hyauns@perfumesjj.com>
2025-03-26 20:29:25 -0800

    Add files via upload

---
+++ languages/tr_TR.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'StreamVid','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-09-04 07:54+0000','po-revision-date'=>'2025-01-07 00:59+0000','last-translator'=>'Jane Doe','language-team'=>'Türkçe','language'=>'tr_TR','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.14; wp-6.5.5','x-domain'=>'streamvid','messages'=>['Please enter keywords'=>'Lütfen anahtar kelimeleri girin']];
